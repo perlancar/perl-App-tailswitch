651f8ba873efbc2c8411e3f9b053174215b34e2d
perlancar (on pc-office) <perlancar@gmail.com>
2017-06-09 12:22:58 +0700

    Initial commit

---
+++ lib/App/tailswitch.pm
@@ -0,0 +1,13 @@
+package App::tailswitch;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Tail a file, but switch when another file with newer name appears
+
+=head1 SYNOPSIS
+
+See the included L<tailswitch> script.
+
+=cut
